3fbdbf883d5f4c201dd4ea0a339969b1ce9485b3
Dinusha <dinusha.diss@gmail.com>
2023-04-25 14:58:53 +0100

    Ruby Basics

--- Basics.rb
+++ Basics.rb
@@ -6,3 +6,16 @@ state = gets.upcase!           # upcase for upper case, downcase for lower case
 puts "What's your age?"
 age = Integer(gets.chomp)
 print "Thank you!"   # Does not go to a new line after print
+
+# if/else statement and comparators
+puts "What's your age?"
+age = Integer(gets.chomp)
+if age < 13
+  print "You are a kid."
+elsif 20 > age && age >= 13
+  print "You're a teenager."
+elsif 30 > age && age >= 20
+  print "You're a young adult."
+else
+  print "You're an elder person."
+end
