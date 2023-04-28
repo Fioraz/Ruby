06098535a1dfcbb6cd6b8ab7ae5506fbb9be516a
Dinusha <dinusha.diss@gmail.com>
2023-04-28 23:49:20 +0100

    .each Operator

---
+++ eachOperator.rb
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5]
+ 
+# Use {} instead of do & end keywords
+# one way to loop
+numbers.each { |item| puts item }
+ 
+# another way to loop
+numbers.each do |item|
+  puts item
+end
+
+
