40ae097f14c7ced13b5bb7cabe8c27432adb7966
Dinusha <dinusha.diss@gmail.com>
2023-04-28 23:13:27 +0100

    For Loop

---
+++ for.rb
@@ -0,0 +1,9 @@
+# Prints numbers from 1 to 10 including 10 - as we used only two dots (..)
+for num in 1..10
+    puts num
+  end
+
+# Prints numbers from 1 to 10 NOT including 10 - as we used three dots (...)
+for num in 1...10
+    puts num
+  end
