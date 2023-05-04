7066680dc0a2b24aa86fff618b03bb51672fbe5b
Dinusha <dinusha.diss@gmail.com>
2023-05-04 13:33:26 +0100

    Arrays

---
+++ arrays.rb
@@ -0,0 +1,6 @@
+array1 = [12, 45, 73, 59]
+array2 = ["abc", "def", "ghi"]
+
+# 2D Array
+my_2d_array = ["*", "*", "*", "*"], ["*", "*", "*", "*"], ["*", "*", "*", "*"], ["*", "*", "*", "*"]
+my_2d_array.each {|x| print "#{x}\n"}    # go to a new line after eachitem of the my_2d_array 
