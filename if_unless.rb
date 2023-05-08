663fcb933faaf5640fae30dd2c53f7b45ce7410d
Dinusha <dinusha.diss@gmail.com>
2023-05-08 22:32:36 +0100

    if, unless, and ternary condition in One Liner

--- if_unless.rb
+++ if_unless.rb
@@ -1,8 +1,8 @@
-#if condition
+#if condition in one line
 puts "Hiya" if true
 
-# Unless condition
+# Unless condition in one line
 puts "Hello there" unless false
 
-# Ternary condition
+# Ternary condition in one line
 puts 6 > 0 ? "6 is Greater than 0" : "6 is not greater than 0"
