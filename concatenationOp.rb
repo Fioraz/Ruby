e8ab1597e9c7c729518d4ee5c4842c397dd2dd13
Dinusha <dinusha.diss@gmail.com>
2023-05-08 22:26:05 +0100

    Concatenation Operator

---
+++ concatenationOp.rb
@@ -0,0 +1,17 @@
+# Push new value to the end of the array
+[1, 2, 3] << 4
+# ==> [1, 2, 3, 4]
+
+# Push new string to the end of the first string
+"Yukihiro " << "Matsumoto"
+# ==> "Yukihiro Matsumoto"
+
+
+
+alphabet = ["a", "b", "c"]
+alphabet << "d"
+puts alphabet
+
+caption = "A giraffe surrounded by "
+caption << "weezards!"
+puts caption
