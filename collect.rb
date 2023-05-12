f4e5e6c9167fa607298515b0643b4707a306268f
Dinusha <dinusha.diss@gmail.com>
2023-05-12 14:42:30 +0100

    Collect Method

---
+++ collect.rb
@@ -0,0 +1,11 @@
+# Do the specific task to each and every element
+my_nums = [1, 2, 3]
+my_nums.collect { |num| num ** 2 }
+# ==> [1, 4, 9]
+
+
+
+fibs = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+doubled_fibs = fibs.collect { |num| num * 2 }
+puts doubled_fibs
