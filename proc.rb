a9181862b948ca1e81a915c3a38c5021d0704543
Dinusha <dinusha.diss@gmail.com>
2023-05-13 16:49:50 +0100

    Proc Syntax

--- proc.rb
+++ proc.rb
@@ -6,3 +6,10 @@ round_down = Proc.new { |x| x.floor}
 # The .collect! and .map! methods do the exact same thing
 ints = floats.collect(&round_down)
 print ints
+
+
+cube = Proc.new { |x| x ** 3 }
+[1, 2, 3].collect!(&cube)
+# ==> [1, 8, 27]
+[4, 5, 6].map!(&cube)
+# ==> [64, 125, 216]
