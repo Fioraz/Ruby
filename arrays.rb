753f10bd43fde0486229fe8e12f260adf0267a43
Dinusha <dinusha.diss@gmail.com>
2023-05-17 21:37:21 +0100

    Ruby Arrays and 2D arrays

--- arrays.rb
+++ arrays.rb
@@ -3,4 +3,4 @@ array2 = ["abc", "def", "ghi"]
 
 # 2D Array
 my_2d_array = ["*", "*", "*", "*"], ["*", "*", "*", "*"], ["*", "*", "*", "*"], ["*", "*", "*", "*"]
-my_2d_array.each {|x| print "#{x}\n"}    # go to a new line after eachitem of the my_2d_array 
+my_2d_array.each {|x| print "#{x}\n"}    # go to a new line after each item of the my_2d_array 
